3b132e15186a4f895edbae911aa51700d6e0a586
edbabayan <ebabayan86@gmail.com>
2025-05-01 17:29:51 +0400

    ADD: to convert csv file information to vector and save them in FAISS database locally

---
+++ src/data_transformer/converter.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import pandas as pd
+from loguru import logger
+from dotenv import load_dotenv
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+def create_faiss_index_from_csv(csv_dir: Path, faiss_dir: Path):
+    """
+    Loads a CSV file, generates OpenAI embeddings, and stores them in a FAISS index.
+
+    Args:
+        csv_dir (Path): Path to the input CSV file.
+        faiss_dir (Path): Directory where the FAISS index will be saved.
+    """
+    # Read the data
+    data = pd.read_csv(csv_dir)
+
+    # Initialize the OpenAI embeddings
+    embedding_function = OpenAIEmbeddings(model=CFG.embedding_model)
+
+    # Prepare the texts
+    texts = data['context'].tolist()
+
+    logger.info("Creating FAISS vector store...")
+
+    # Create FAISS vector store
+    db = FAISS.from_texts(texts, embedding=embedding_function)
+
+    # Save the FAISS index
+    db.save_local(str(faiss_dir))
+    logger.success(f"FAISS index saved to {faiss_dir}")
+
+
+if __name__ == '__main__':
+    from src.config import CFG
+
+    # Load environment variables
+    load_dotenv(dotenv_path=CFG.env_variable_file)
+
+    create_faiss_index_from_csv(CFG.csv_dir, CFG.faiss_dir)
