b4fa38dc518a79b2dd663387885098f6fe2bb1a5
edbabayan <ebabayan86@gmail.com>
2025-05-01 17:29:21 +0400

    ADD: env_variable_file, faiss_dir, embedding_model

--- src/config.py
+++ src/config.py
@@ -4,5 +4,9 @@ from pathlib import Path
 class CFG:
     root = Path(__file__).parent.parent
 
+    env_variable_file = root / '.env'
     data_dir = root / 'data'
     csv_dir = data_dir / 'renewable_energy_topics.csv'
+    faiss_dir = data_dir / 'faiss_index'
+
+    embedding_model = "text-embedding-3-large"
