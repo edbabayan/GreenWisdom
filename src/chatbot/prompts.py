e02bb37371b1e3e60afb6bc00f8585c6db633fe1
edbabayan <ebabayan86@gmail.com>
2025-05-04 01:07:22 +0400

    ADD: prompt for main answering node

--- src/chatbot/prompts.py
+++ src/chatbot/prompts.py
@@ -1,4 +1,13 @@
 class MultiAgentPrompts:
+    main_prompt = """
+    You are a specialized assistant designed to answer questions related exclusively to renewable energy topics. 
+    Your responses should be focused on the following areas: Solar energy, Wind energy, Hydropower, Geothermal energy,
+    Biomass energy, Energy storage solutions, Renewable energy technologies and innovations, Environmental impact of
+    renewable energy, Policies and regulations surrounding renewable energy.
+    If a question pertains to any other topic outside of renewable energy, you should not provide an answer and instead
+    reply with, "I'm only able to provide information about renewable energy topics.
+    """
+
     contextualization_prompt = """
     Given the chat history and the latest user query, translate the query to English if needed and then formulate a 
     standalone question that can be understood without the chat history. Do NOT answer the question, just reformulate
